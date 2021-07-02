fa0d52c25d54b61f9dd933d48a4d493738473016
IeSua-lab <66435293+IeSua-lab@users.noreply.github.com>
2021-07-02 17:53:22 +0300

    Add files via upload

---
+++ snapshot.sh
@@ -0,0 +1,10 @@
+systemctl stop joystream-node
+sudo apt-get update
+sudo rm -rf /home/$USER/.local/share/joystream-node/chains/joy_testnet_5/db/
+sudo apt install python-pip
+sudo pip install gdown
+gdown --id 1bnhEkX1L0CBIodEVUUW7t86I_FNAYC6C
+7z x snapshot_02072021_block1234430.7z -o/home/$USER/.local/share/joystream-node/chains/joy_testnet_5/
+sudo rm -rf snapshot_02072021_block1234430.7z
+sudo systemctl start joystream-node
+sudo systemctl enable joystream-node
