9b88359a35397e63cad59787ef5ce8cf8cd84120
IeSua-lab <66435293+IeSua-lab@users.noreply.github.com>
2021-07-02 18:11:13 +0300

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -2,7 +2,7 @@ systemctl stop joystream-node
 sudo apt-get update
 sudo rm -rf /home/$USER/.local/share/joystream-node/chains/joy_testnet_5/db/
 sudo rm -rf /root/.local/share/joystream-node/chains/joy_testnet_5/db/
-sudo apt install python-pip
+sudo apt-get install python3-pip
 sudo pip install gdown
 gdown --id 1bnhEkX1L0CBIodEVUUW7t86I_FNAYC6C
 7z x snapshot_02072021_block1234430.7z -o/home/$USER/.local/share/joystream-node/chains/joy_testnet_5/
